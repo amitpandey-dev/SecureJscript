bae3d6e3712f3a7c92521e2d09192d257638c7dd
amitpmaster18 <amitp.master18@outlook.com>
2021-05-23 23:30:00 +0530

    Very first code change for concepts for javascript security on client.

---
+++ src/ngscripts/scriptAbout.js
@@ -0,0 +1,6 @@
+﻿angular.module('aboutApp', [])
+    .controller('detailController', ['$scope', function ($scope) {
+        'use strict';
+        $scope.phone = '01-654-2276';
+        $scope.email = 'amitpandey.void@example.com';
+    }]);   
---
+++ src/ngscripts/scriptIndex.js
@@ -0,0 +1,7 @@
+﻿angular.module('indexapp', [])
+    .controller('indexController', ['$scope', function ($scope) {
+        'use strict';
+        
+        $scope.para = 'ASP.NET MVC with AngularJS script minifiy  functionality';
+        $scope.email = 'amitpandey.void@example.com'
+    }]);   
